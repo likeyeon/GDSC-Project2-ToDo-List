c317632d7c0cecd14d6ed2fe7b0a83e0c8433382
likeyeon <chy98csy00@naver.com>
2022-11-18 18:01:05 +0900

    feat: 기능 구현

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import appStyle from "./style/hellow.module.css"
-
-const App = document.getElementById("app")
-
-App.classList.add(appStyle.appFont)
-App.innerHTML = `<div class="hellow">Vanilla template</div>`
